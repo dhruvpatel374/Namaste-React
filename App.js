b53bea4ef508d8ad2c4691f1c610db0608e5f41b
creator2104 <vinitr.prajapati2104@gmail.com>
2025-03-18 13:16:06 +0530

    updated and created new files

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 const heading = React.createElement(
 "h1",
-// react element is a js object
+// create element belongs from react
+// react element(heading) is just a js object
 { id: "heading", xyz: "abc" },  "Hello from react!");
 // above line is known as props and "hello from react" is children which will go inside h1 tag 
 // id and xyz is known as attributes
-// create element belongs from react
+// console.log(heading);  //object
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // create root belongs from react DOM so we have to create root from ReactDOM
 root.render(heading);
-// render method convert the object into html h1 tag while rendering 
 // attach heading into the root
+// render method convert the object into html h1 tag while rendering 
+//ReactElement(object) => HTML(Browser Understands)
+
 
 
 // <div id="parent">
@@ -23,6 +26,8 @@ root.render(heading);
 // </div>
 // </div>
  
+//if u have element siblings then write it in array format 
+
 // const parent = React.createElement("div",{id:"parent"},
 //    [ 
 //      React.createElement("div",{id:"child"},
@@ -30,6 +35,9 @@ root.render(heading);
 //      React.createElement("div",{id:"child"},
 //      [React.createElement("h1",{},"I'm an h1 tag"),React.creteElement("h2",{},"I'm an h2 tag") ])
 //    ])   
+
+// to remove above array kinda complexity use jsx  
+
 // console.log(parent);   // it gives an object at output
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(parent) 
